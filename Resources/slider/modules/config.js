7c08272e11a94070c41f524105c74d2815ab0608
G-grbz <gkhn.gurbuz@hotmail.com>
2026-06-11 02:04:40 +0300

    update to v3.7.0

--- Resources/slider/modules/config.js
+++ Resources/slider/modules/config.js
@@ -779,7 +779,7 @@ export function getConfig() {
     useRandomContent: localStorage.getItem('useRandomContent') !== 'false',
     fullscreenMode: localStorage.getItem('fullscreenMode') === 'true' ? true : false,
     listLimit: 20,
-    version: "v3.6.0",
+    version: "v3.7.0",
     historySize: 20,
     updateInterval: 300000,
     nextTracksSource: localStorage.getItem('nextTracksSource') || 'playlist',
